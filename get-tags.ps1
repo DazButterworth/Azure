845cb565210a38c9c48ef173f10f3090714b11d8
Darren Butterworth (Wintel - Shared Services IT) <ButtDa@live.co-op.local>
2017-02-11 01:08:37 +0000

    Initial Commit - v1

---
+++ get-tags.ps1
@@ -0,0 +1,32 @@
+﻿
+param(
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline =$true,
+        ValueFromPipelineByPropertyName =$true,
+        HelpMessage = "One or more computer names, separated by commas, followed by a space and then the Resource Group Name"
+    )]
+    [string[]]$VMNames,
+    [string]$ResGroupVM = "SS-INFRA-PROD-EUN-RG"
+)
+
+#Import-module AzureRM
+
+#To logon & connect to Azure
+Login-AzureRmAccount
+
+#For formating purposes only
+Write-Host
+
+#Step through each VMName provided
+ForEach ($VMName in $VMNames){
+    #Get VM as an object
+    $vm = get-azurermvm -name $VMName -resourcegroupname $ResGroupVM -WarningAction SilentlyContinue
+    
+    #Display Name and Tags
+    $vm.Name
+    $vm.Tags | Format-Table
+    
+    #To list a specific tag
+    # $vm.tags['billTo']
+}
---
+++ update-tag.ps1
@@ -0,0 +1,66 @@
+﻿# This script replaces a certain value with a new value on a specified Azure VM Tag
+
+param(
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline =$true,
+        ValueFromPipelineByPropertyName =$true,
+        HelpMessage = "Tag to be updated. eg: ./update-tag.ps1 NominalLedger fdssdfsfd 444"
+    )]
+    [string]$Tag,
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline =$true,
+        ValueFromPipelineByPropertyName =$true,
+        HelpMessage = "Old Value. eg: ./update-tag.ps1 NominalLedger fdssdfsfd 444"
+    )]
+    [string]$OldValue,
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline =$true,
+        ValueFromPipelineByPropertyName =$true,
+        HelpMessage = "New Value. eg: ./update-tag.ps1 NominalLedger fdssdfsfd 444"
+    )]
+    [string]$NewValue
+)
+
+#To logon & connect to Azure
+Login-AzureRmAccount
+
+#Include if module not already installed - takes time to load
+# Import-module AzureRM
+
+#Tags are updated as a single object. To add a tag to a resource that already includes tags,
+#first retrieve the existing tags. Add the new tag to the object that contains the existing tags,
+#and reapply all the tags to the resource.
+
+#Get all VMs in Azure
+$VMs = Get-AzureRmVM
+
+#Step through each VM
+ForEach ($VM in $VMs)
+    {
+    #Get individual VM's tags
+    $tags = $VM.Tags
+    #Check to see if specified Tag has specified Value
+    if ($tags[$Tag] -match $OldValue)
+        {
+        $VMName = $VM.Name
+        $ResGroupVM = $VM.ResourceGroupName
+        #For formatting only
+        Write-Host
+        #Display VM and current Tag Value
+        write-host($VMName + "`t" + $tags[$Tag])
+        #Remove whole Tag not just Value
+        $tags.remove($Tag)
+        #Re-add Tag but with new Value
+        $tags.add($Tag,$NewValue)
+        #Display New Tag Value
+        write-host($VMName + "`t" + $tags[$Tag])
+        #Following command applies the asjusted tags in one go
+        #Comment next line out to test which VMs are selected and to check Tag gets updated and, only then, remove the comment
+        Set-AzureRmResource -ResourceGroupName $ResGroupVM -Name $VMName  -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force
+        }
+    }
+
+
