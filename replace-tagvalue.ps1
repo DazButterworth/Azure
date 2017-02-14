99b7e173ad890d67ca869110c974a701c05c9c84
Darren Butterworth (Wintel - Shared Services IT) <ButtDa@live.co-op.local>
2017-02-14 16:31:16 +0000

    updates to usage

---
+++ replace-tagvalue.ps1
@@ -0,0 +1,23 @@
+﻿#Login-AzureRmAccount
+
+$tag = "SRID"
+$value = "Automated"
+$value_new = "Portal Build"
+$VMs = Get-AzureRmVM
+ForEach ($VM in $VMs)
+    {
+    #$vm.name
+    #$vm.Tags | Format-Table
+    #$vm.Tags[$tag]
+    $tags = $vm.Tags
+    if ($tags[$tag] -eq $value)
+        {
+        $ResGroupVM = $vm.ResourceGroupName
+        write-host($vm.Name + "`t" + $tags[$tag])
+        $tags.remove($tag)
+        $tags.add($tag,$value_new)
+        write-host($vm.Name + "`t" + $tags[$tag])
+        #Set-AzureRmResource -ResourceGroupName $ResGroupVM -Name $vm.name -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force
+        }
+    write-host
+    }
